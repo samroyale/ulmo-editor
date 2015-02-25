f2c394c0e9b0468ac1592f4bcb8764a8ce6b716c
Sam Eldred <samuel.eldred@gmail.com>
2015-02-25 16:16:41 +0000

    Renamed run scripts

--- linux_run.sh
+++
@@ -1,1 +0,0 @@
-java -cp ./build/libs/editor-1.0.jar:./lib/swt-linux-gtk-64-4.3.jar:./src/test/resources/ rpg.editor.RpgEditor
--- mac_run.sh
+++
@@ -1,1 +0,0 @@
-java -XstartOnFirstThread -cp ./build/libs/editor-1.0.jar:./lib/swt-macosx-cocoa-64-4.3.jar:./src/test/resources/ rpg.editor.RpgEditor
